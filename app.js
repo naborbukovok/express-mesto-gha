bfebf69f3c64ecda822bdbeade71d222eba78129
maria <mdmolokova@edu.hse.ru>
2023-10-01 23:31:56 +0300

    Fixed error handling at user creation

--- app.js
+++ app.js
@@ -33,6 +33,9 @@ app.post('/signin', celebrate({
 
 app.post('/signup', celebrate({
   body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string(),
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
   }),
