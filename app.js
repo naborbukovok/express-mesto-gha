c4a9eec2dcf482bee984e7479bb79bf154bb2925
maria <mdmolokova@edu.hse.ru>
2023-10-02 09:57:11 +0300

    Small fixes

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const auth = require('./middlewares/auth');
 const { login, createUser } = require('./controllers/users');
 const { validateURL } = require('./utils/validators');
 const NotFoundError = require('./errors/not-found-error');
+const errorHandler = require('./middlewares/error-handler');
 
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 const app = express();
@@ -53,11 +54,7 @@ app.use(() => {
 
 app.use(errors());
 
-app.use((err, req, res, next) => {
-  const { statusCode = 500, message } = err;
-  res.status(statusCode).send({ message: statusCode === 500 ? 'На сервере произошла ошибка.' : message });
-  next();
-});
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`PORT: ${PORT}`);
--- models/user.js
+++ models/user.js
@@ -35,7 +35,6 @@ const userSchema = new mongoose.Schema({
   },
   password: {
     type: String,
-    minlength: 8,
     required: true,
     select: false,
   },
